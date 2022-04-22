b693f9d8687347c38e85ab3be10fcacbf64a20f6
afranio-viana <afranio.a164@gmail.com>
2022-04-22 18:40:36 -0400

    .

--- src/index.js
+++ src/index.js
@@ -18,4 +18,4 @@ app.use(express.json())
 app.use(routes)
 const port = process.env.PORT || 3000 ;
 
-app.listen(port, () =>console.log('Server running!!!'))
+app.listen(port, () =>console.log('Server running!'))
