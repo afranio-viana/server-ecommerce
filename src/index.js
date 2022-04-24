807a3264bb2ddc72e9591be4e6921b224e7de58c
afranio-viana <afranio.a164@gmail.com>
2022-04-24 12:52:13 -0400

    subindo para o heroku

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const dotenv=require('dotenv')
 const cors = require('cors')
 const routes =require('./routes')
 
-dotenv.config()
+require('dotenv').config()
 
 const app = express()
-const MONGO_URI=process.env.MONGO_URI
-mongoose.connect(MONGO_URI,{
+
+mongoose.connect(process.env.MONGO_URI,{
     useUnifiedTopology: true,
     useNewUrlParser: true
 },console.log('Conectado com o banco'))
@@ -18,4 +17,4 @@ app.use(express.json())
 app.use(routes)
 const port = process.env.PORT || 3000 ;
 
-app.listen(port, () =>console.log(typeof MONGO_URI))
+app.listen(port, () =>console.log('Server running!'))
